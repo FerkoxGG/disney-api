e5027f88d4764599f5753dcdb0921a6ac2682b16
Fernando Gonzalez <fernandogonzalezgaete@gmail.com>
2022-02-25 00:44:21 -0300

    movie routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :characters, only: [:index, :create, :update, :destroy]
+      resources :movie_or_series, only: [:index, :create, :update, :destroy]
     end
   end
 end
