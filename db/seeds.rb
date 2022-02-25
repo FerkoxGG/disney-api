d649be8b77ba15d9f3364c66fc2dafdd15e2b45e
Fernando Gonzalez <fernandogonzalezgaete@gmail.com>
2022-02-24 22:09:05 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,13 +9,6 @@ user = User.find_or_initialize_by(email: 'fernandogonzalezgaete@gmail.com')
 user.password = 'contraseña'
 user.password_confirmation = 'contraseña'
 
-character = Character.new(name: 'Mickey Mouse', image: 'https://static.wikia.nocookie.net/doblaje/images/3/3a/Mickey_mouse-1097.jpg/revision/latest/scale-to-width-down/342?cb=20130925015641&path-prefix=es')
-character2 = Character.new(name: 'Goofy', image: 'https://static.wikia.nocookie.net/disney/images/e/e5/Goofy.png/revision/latest?cb=20140306174418&path-prefix=es')
-character3 = Character.new(name: 'Donald Duck', image: 'https://static.wikia.nocookie.net/disney/images/6/6f/Donald_Duck.png/revision/latest/top-crop/width/360/height/450?cb=20140427112158&path-prefix=es')
-
 user.save
-character.save
-character2.save
-character3.save
 
 p "Seed creado"
